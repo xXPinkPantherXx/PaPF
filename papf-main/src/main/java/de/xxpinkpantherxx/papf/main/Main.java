001ec1ccb092ceb9c9aa898c564f8235a8bf9702
xXPinkPantherXx <momme.r@hotmail.de>
2018-09-23 19:35:27 +0200

    ApplicationHelper - Localization Fixup2

--- papf-main/src/main/java/de/xxpinkpantherxx/papf/main/Main.java
+++ papf-main/src/main/java/de/xxpinkpantherxx/papf/main/Main.java
@@ -7,7 +7,6 @@ public class Main {
     private final static String MAINE_MESSAGE_KEY = "main.main";
     
     public static void main(String[] args) {
-//        ApplicationHelper.init();
         final String output = ApplicationHelper.getMessage(MAINE_MESSAGE_KEY);
         System.out.println(output);
     }
